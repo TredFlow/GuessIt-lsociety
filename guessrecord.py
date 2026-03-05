514cadbeabcfda529e529ac7b59a0279033df203
TredFlow <artsut2011@gmail.com>
2026-03-05 07:55:12 +0300

    Update guessrecord.py
    Added localization to the languess module.

--- guessrecord.py
+++ guessrecord.py
@@ -1,5 +1,6 @@
 import os
 from art import tprint
+from languess import Translations
 def record_check():
     file_name = 'record.txt'
     if  not os.path.exists(file_name ):
@@ -14,15 +15,15 @@ def record_mod(new_record):
     with open ('record.txt', 'w') as f:
        f.write(str(new_record))
 
-def record_guess(attempts, current_record):
+def record_guess(attempts, current_record, lang):
     attempts = int(attempts)
     current_record = int(current_record)
     if attempts == current_record:
-        print('You can always be thinner. Look Better. - P.Bateman')
-        tprint('LOOK BETTER')
+        print(Translations [lang]['quote'])
+        tprint(Translations [lang]['look'])
     elif attempts < current_record:
-        print(f'Cool, Your new record is {current_record - attempts} attempts better. ')
-        record_mod(attempts)
+        print(Translations [lang]['new_record'].format(current_record - attempts))
+        record_mod
     elif attempts > current_record:
-        print(f'Bro, you were better last time. Your best was {current_record}.')
+        print(Translations [lang]['bad_record'].format(attempts))
         
