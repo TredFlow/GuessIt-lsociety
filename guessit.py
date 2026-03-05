acb9288c81150474d35c7fa02ece2edcbbf9393d
TredFlow <artsut2011@gmail.com>
2026-03-05 07:54:26 +0300

    Update guessit.py
    Added localization to the 'languess' module. Fixed a small bug.

--- guessit.py
+++ guessit.py
@@ -2,35 +2,41 @@ import random
 import time
 from modguess import slow_print, messages, tprint
 from guessrecord import record_check, record_guess
+from languess import Translations
+lang = input("Choose language / Выбери язык (en/ru): ").lower()
+if lang not in ['ru', 'en']: 
+    lang = 'en' 
 best_score = record_check()
 num = random.randint(1, 100)
 attempts = 0
-slow_print('Welcome to the game!')
+slow_print(Translations [lang]['start'])
 time.sleep(1)
 tprint('GUESS IT')
 time.sleep(2)
-slow_print(f"Current record to beat: {best_score} attempts")
+slow_print(Translations [lang]['record_status'].format(best_score))
 time.sleep(1)
 while True:
-    guess = input('Enter a number between 1 and 100:')
+    guess = input(Translations [lang]['guess'])
     attempts += 1
     if not guess.isdigit(): 
-        print("Enter the NUMBERS, bro")
+        print(Translations [lang]['numbers_only'])
         continue
     i = int(guess)
     if i < 1 or i > 100: 
-        slow_print('The number must be between 1 and 100!')
+        slow_print(Translations [lang]['range'])
         continue
     if i == num:
-        messages(attempts)
+        messages(attempts, lang)
         break
     elif i < num:
-        slow_print('The number is higher!')
+        slow_print(Translations [lang]['higher'])
     elif i > num:
-        slow_print('The number is less!')
+        slow_print(Translations [lang]['less'])
 
-record_guess(attempts, best_score)
-Exit = input('Press Enter to exit...')
+record_guess(attempts, best_score, lang)
+
+Exit = input(Translations [lang]['exit'])
+
 
 
 
