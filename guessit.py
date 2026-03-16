d14ff751f8779c060fb047b3a5e8120a03ad94b1
TredFlow <artsut2011@gmail.com>
2026-03-16 22:02:45 +0300

    Update guessit.py
    Added difficulty level selection

--- guessit.py
+++ guessit.py
@@ -1,15 +1,13 @@
 import random
 import time
-from modguess import slow_print, messages, tprint
+from modguess import slow_print, messages, tprint, level_status
 from guessrecord import record_check, record_guess
 from languess import Translations
-# TODO: Add difficulty levels in v1.4
 lang = input("Choose language / Выбери язык (en/ru): ").lower()
 if lang not in ['ru', 'en']: 
     lang = 'en' 
 name = input(Translations [lang]['name'])
 best_score = record_check()
-num = random.randint(1, 100)
 attempts = 0
 slow_print(Translations [lang]['start'].format(name))
 time.sleep(1)
@@ -18,8 +16,12 @@ time.sleep(1)
 slow_print(Translations [lang]['record_status'].format(best_score))
 slow_print(Translations [lang]['stat_clue'])
 time.sleep(1)
+level = input(Translations [lang]['level_status'])
+max_num = level_status(level)
+num = random.randint(1, max_num)
+time.sleep(1)
 while True:
-    guess = input(Translations [lang]['guess'])
+    guess = input(Translations [lang]['guess'].format(max_num))
     if guess in ['stat']:
         print(Translations [lang]['stat_user'])
         with open ('record.txt','r') as f:
@@ -30,8 +32,8 @@ while True:
         print(Translations [lang]['numbers_only'])
         continue
     i = int(guess)
-    if i < 1 or i > 100: 
-        slow_print(Translations [lang]['range'])
+    if i < 1 or i > max_num: 
+        slow_print(Translations [lang]['range'].format(max_num))
         continue
     if i == num:
         messages(attempts, lang)
@@ -44,8 +46,7 @@ while True:
 record_guess(name, attempts, best_score, lang)
 
 Exit = input(Translations [lang]['exit'])
-
-
+    
 
 
 
